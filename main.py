1b2c747927980da5a82456e7fbb93f2e7ce21340
roma <bshs71734@gmail.com>
2024-10-02 10:06:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import math
 V = float(input("Введите объем сферы "))
-if V < 0:
-    print("введите корректные значения")
-else:
-       radius = (3 * V ** (1/3)) / (4 * math.pi)
-       print(f"Радиус сферы: {radius:}")
+radius = (3 * V ** (1/3)) / (4 * math.pi)
+print(f"Радиус сферы: {radius:}")
